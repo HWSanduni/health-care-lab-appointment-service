36e90b6335da27af7f6678b577ae094c423ceeef
Sanduni Shashikala <sanduni.shashikala@digiratina.com>
2024-02-24 21:04:18 +0530

    Added Database tables and changes

--- src/main/java/com/health/care/lab/appointment/entity/Report.java
+++ src/main/java/com/health/care/lab/appointment/entity/Report.java
@@ -1,8 +1,35 @@
 package com.health.care.lab.appointment.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "report")
 public class Report {
-  private int id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "content")
   private String content;
-  private String patientId;
-  private String doctorId;
-  private String technicianId;
+
+  @ManyToOne
+  @JoinColumn(name="patient_Id", nullable=false)
+  private Patient patient;
+
+  @ManyToOne
+  @JoinColumn(name="doctor_Id", nullable=false)
+  private Doctor doctor;
+
+  @ManyToOne
+  @JoinColumn(name="technician_Id", nullable=false)
+  private Technician technician;
+
 }
